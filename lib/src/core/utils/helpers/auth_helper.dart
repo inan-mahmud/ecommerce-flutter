591d8d5774d5ba70881a188eba985ced4af972da
Inan Mahmud <inan.mahmud@cefalo.com>
2023-05-16 19:58:13 +0600

    add authhelper

---
+++ lib/src/core/utils/helpers/auth_helper.dart
@@ -0,0 +1,12 @@
+import 'package:ecommerce_flutter/src/core/di/locator.dart';
+import 'package:ecommerce_flutter/src/core/services/cache/cache_interface.dart';
+
+const authToken = 'token';
+
+class AuthHelper {
+  static final cacheService = locator<CacheInterface>();
+
+  static Future<bool> saveToken(String token) async {
+    return await cacheService.save(key: authToken, value: token);
+  }
+}
