dd5b485bf6de7a28c3a2915f0be3a7c9ff28b6d0
Inan Mahmud <inan.mahmud@cefalo.com>
2023-05-17 13:05:07 +0600

    add configs

---
+++ lib/src/core/config/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color greyColor = Color(0xFF7886A8);
+  static const Color backgroundColor = Color(0xFF0C111E);
+  static const Color purpleColor = Color(0xFF6F66F6);
+  static const Color greenColor = Color(0xFF05AF86);
+  static const Color textFieldColor = Color(0xFF161D30);
+  static const Color appBarColor = Color(0xFF182734);
+}
---
+++ lib/src/core/config/theme.dart
@@ -0,0 +1,58 @@
+import 'package:ecommerce_flutter/src/core/config/app_sizes.dart';
+import 'package:ecommerce_flutter/src/core/config/colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData dark = ThemeData(
+    appBarTheme: const AppBarTheme(
+      color: AppColors.appBarColor,
+      titleTextStyle: TextStyle(
+        color: AppColors.appBarColor,
+        fontWeight: FontWeight.bold,
+        fontSize: Sizes.p24,
+      ),
+      centerTitle: true,
+    ),
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColors.backgroundColor,
+    snackBarTheme: const SnackBarThemeData(
+      actionTextColor: AppColors.whiteColor,
+      backgroundColor: AppColors.purpleColor,
+      contentTextStyle: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+    focusColor: Colors.black,
+    primaryColor: AppColors.greenColor,
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(
+          color: Colors.white, fontSize: 36, fontWeight: FontWeight.w500),
+      displayMedium: TextStyle(
+          color: Colors.white, fontSize: 34, fontWeight: FontWeight.w400),
+      displaySmall: TextStyle(
+          color: Colors.white, fontSize: 22, fontWeight: FontWeight.w500),
+      headlineMedium: TextStyle(
+          color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500),
+      headlineSmall: TextStyle(
+          color: Colors.white, fontSize: 19, fontWeight: FontWeight.w700),
+      titleLarge: TextStyle(
+          color: Colors.white, fontSize: 18, fontWeight: FontWeight.w600),
+      bodyLarge: TextStyle(
+          color: AppColors.blackColor,
+          fontSize: 14,
+          fontWeight: FontWeight.w500),
+      bodyMedium: TextStyle(
+          color: AppColors.blackColor,
+          fontSize: 14,
+          fontWeight: FontWeight.w700),
+      titleMedium: TextStyle(
+          color: AppColors.blackColor,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+      titleSmall: TextStyle(
+          color: AppColors.blackColor,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+  );
+}
